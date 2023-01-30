e840bdb593cba0d32988e37a2e36a957883c2a59
Denis Johnson <G00403126@atu.ie>
2023-01-30 12:17:41 +0000

    Part1

--- src/main/java/ie/atu/ArrayDemo.java
+++ src/main/java/ie/atu/ArrayDemo.java
@@ -1,4 +1,41 @@
 package ie.atu;
 
 public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] arrays;
+        arrays = new int[10];
+
+        arrays[0] = 100;
+        System.out.println("Element at index 0: " + arrays[0]);
+
+        arrays[1] = 200;
+        System.out.println("Element at index 1: " + arrays[1]);
+
+        arrays[2] = 300;
+        System.out.println("Element at index 2: " + arrays[2]);
+
+        arrays[3] = 400;
+        System.out.println("Element at index 3: " + arrays[3]);
+
+        arrays[4] = 500;
+        System.out.println("Element at index 4: " + arrays[4]);
+
+        arrays[5] = 600;
+        System.out.println("Element at index 5: " + arrays[5]);
+
+        arrays[6] = 700;
+        System.out.println("Element at index 6: " + arrays[6]);
+
+        arrays[7] = 800;
+        System.out.println("Element at index 7: " + arrays[7]);
+
+        arrays[8] = 900;
+        System.out.println("Element at index 8: " + arrays[8]);
+
+        arrays[9] = 1000;
+        System.out.println("Element at index 9: " + arrays[9]);
+
+
+    }
 }
